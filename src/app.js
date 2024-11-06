3dbc0ed7330b06c8edacbc08d3f82e22cb8e5db1
moises2872024 <moises.montt287@gmail.com>
2024-11-06 19:01:57 +0000

    random-card finalizado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const suits = ["spade", "heart", "club", "diamond"];
+
+function getRandomSuit() {
+  const randomIndex = Math.floor(Math.random() * suits.length);
+  return suits[randomIndex];
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const card = document.getElementById("randomCard");
+  const randomSuit = getRandomSuit();
+  card.classList.add(randomSuit);
+  const randomNumber = getRandomNumber();
+  const cardNumberElement = document.getElementById("cardNumber");
+  cardNumberElement.textContent = randomNumber;
 };
